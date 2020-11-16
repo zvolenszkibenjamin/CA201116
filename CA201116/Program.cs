1ec15288dc954f085678185efd571a5ff715175d
Benjámin Zvolenszki <billostudio1@gmail.com>
2020-11-16 08:48:08 +0100

    Initial commit

---
+++ CA201116/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CA201116
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Feladat
+            /*
+             * Az egitestek.txt fájlban a Naprendszer égitesteinek adatai találhatók, soronként a következő adatokkal:
+             * az égitest neve, annak az égitestnek a neve, ami körül kering, a keringési távolság,
+             * az égitest átmérője. Az egy sorban szereplő különböző adatokat szóköz választja el.
+             * 
+             * Olvasd be a fájl tartalmát, majd írja a képernyőre a következő kérdésekre adott válaszokat:
+             * a) Mekkora az égitestek átlagos mérete?
+             * b) Melyik a legnagyobb átmérőjű égitest?
+             * c) Hány holdja van a Neptunusznak (azaz hány égitest kering körülötte)?
+             * d) Melyek a Naprendszerünk bolygói (mely égitestek keringenek a Nap körül)?
+             * e) Válogasd ki egy listába a Jupiter holdjait, majd rendezd a tömböt az égitestek keringési távolsága szerint növekvő sorrendbe és írasd ki a jupiter.txt fájlba.
+             */
+            
+            const string resPath = @"..\..\Resources";
+        }
+    }
+}
